58a03c93fcbd02f42885167618c092f1767e33ec
jagasia <diptopravajana@gmail.com>
2022-07-18 14:56:36 +0530

    dj

---
+++ frontend/jst-foods/src/app/customer-login/customer-login.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RouterLinkWithHref } from '@angular/router';
+import { CustomerRegistrationService } from '../custome-registration.service';
+
+@Component({
+  selector: 'app-customer-login',
+  templateUrl: './customer-login.component.html',
+  styleUrls: ['./customer-login.component.css']
+})
+export class CustomerLoginComponent implements OnInit {
+customerLoginForm:FormGroup;
+isLogin = false;
+o:any;
+  constructor(private fb:FormBuilder,private cs:CustomerRegistrationService) { 
+    this.customerLoginForm=this.fb.group({
+    // id:[null, Validators.required],
+   id:[null, Validators.required],
+    password:[null, Validators.required]
+    
+  }); }
+  ngOnInit(): void {
+  }
+    fnsubmit()
+    {
+      if(this.customerLoginForm.invalid){
+        alert('Valid login credential required')
+        return;
+      } 
+      alert("hi");
+      this.o=this.cs.fnLogin(this.customerLoginForm.value).subscribe((data)=>console.log(data));
+      if(this.o!=null)
+      {
+        alert("u are logged in");
+         
+      }
+    }
+    
+  }
+ 
+  
+
+
+
---
+++ frontend/jst-foods/src/app/customer-registration/customer-registration.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomerRegistrationService } from '../custome-registration.service';
+
+@Component({
+  selector: 'app-customer-registration',
+  templateUrl: './customer-registration.component.html',
+  styleUrls: ['./customer-registration.component.css']
+})
+export class CustomerRegistrationComponent implements OnInit {
+  customerRegistrationForm: FormGroup;
+  o: any;
+  constructor(private fb: FormBuilder, private crs: CustomerRegistrationService) {
+    this.customerRegistrationForm = this.fb.group({
+      id:[''],
+      contact: ['', [Validators.required, Validators.min(10)]],
+      email: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      cpassword: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  fnSubmit() {
+    if(this.customerRegistrationForm.invalid){
+      console.log(this.customerRegistrationForm);
+      
+      alert('Customer valid details are required');
+      return;
+    }
+      this.o=this.crs.fnCreate(this.customerRegistrationForm.value).subscribe((data:any)=>{
+        console.log(data)
+        //  alert(this.customerRegistrationForm.id);
+          if(this.o!=null)
+        {  
+            alert("Sign-up successfull: You can login and Enjoy  ");
+          alert(`login with id ${data.id}`);
+          localStorage.setItem('userdetails', JSON.stringify(data))
+    
+          }
+      
+      });
+    
+  }
+}
